440d6f2ec7cbf019026e176749f1754fa6d21cd2
Vishil Patel <112727690+vishilpatel1@users.noreply.github.com>
2022-09-09 22:41:35 +0000

    I made the purple turtle go around a square :)

---
+++ ch02/exercises/myturtle.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.shape('turtle')
+turtle.color('purple')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.exitonclick()
--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,22 @@
 
 #Part A
 weeks = 16
+print(weeks, type(weeks))
 classes = 5
+print(classes, type(classes))
 tuition = 6000
+print(tuition, type(tuition))
 cost_per_week = ((tuition / classes) / weeks)
+print(cost_per_week, type(cost_per_week))
 print("Cost per week:", cost_per_week)
-
+classes_per_week = 3
+print(classes_per_week, type(classes_per_week))
+cost_per_class = cost_per_week/classes_per_week
+print ('the cost per class is', cost_per_class)
+print(cost_per_class, type(cost_per_class))
 
 #Part B
+import random
+my_list = ["rock", "paper", "scissors", "fire", "water"]
+random_word = random.choice(my_list)
+print (random_word)
